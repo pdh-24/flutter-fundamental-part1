fa584d7a50e9796c36f8f7d5ad7f5469eef63a9a
muhammad-paksi <paksi.home@gmail.com>
2024-10-27 11:47:48 +0700

    Screemshot

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,8 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              // 'You have pushed the button this many times:',
+              'Muhammad Paksi Satrio Bayu Dhiyaul Haq'
             ),
             Text(
               '$_counter',
